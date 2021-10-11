892882b9dd9d73bcd9cdebbdffe7bd252a48146d
root <root@DESKTOP-M2D1LCR.localdomain>
2021-10-11 17:50:40 +0000

    new changes

--- src/com/company/src/com/company/MembershipType.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public abstract class MembershipType {
-    public int maxTrade;
-    public abstract String getMembershipName();
-}
-
-
--- src/com/company/src/com/company/labwork/Nameable.java
+++
@@ -1,5 +0,0 @@
-package com.company.labwork;
-
-public interface Nameable {
-     String getName();
-}
